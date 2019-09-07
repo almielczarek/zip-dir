a96284153accf784f91dbc93491a983d30625302
Alexander Mielczarek <almielczarek@gmail.com>
2019-09-07 12:53:10 -0500

    Add example to documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! # Example
+//! ```
+//! use std::fs::File;
+//! use std::io;
+//! use std::path::Path;
+//! 
+//! use zip_dir::zip_dir;
+//! 
+//! fn run() -> io::Result<()> {
+//!     let path = Path::new(".");
+//!     let file = File::create("archive.zip")?;
+//! 
+//!     // See http://mvdnes.github.io/rust-docs/zip-rs/zip/write/struct.FileOptions.html for options
+//!     // Passing None uses the default options with no compression
+//!     let options = None;
+//! 
+//!     // zip_dir returns its second parameter wrapped in a Result if successful
+//!     let _file = zip_dir(path, file, options)?;
+//! 
+//!     Ok(())
+//! }
+//!
+//! fn main() {
+//!     match run() {
+//!         Ok(()) => println!("Wrote archive.zip"),
+//!         Err(e) => eprintln!("Error: {}", e)
+//!     }
+//! }
+//! ```
+
 use std::fs::{read_dir, DirEntry, File};
 use std::io::{self, Read, Seek, Write};
 use std::path::Path;
